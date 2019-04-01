9bf790348f8a87faec9baec36e3aaa7e70b30230
calebwin <calebhwin@gmail.com>
2019-03-31 23:34:11 -0700

    added golang example

---
+++ playground/basic.go
@@ -0,0 +1,19 @@
+// you can try this out as follows:
+// 	cd stdg
+// 	cargo build
+// 	go run playground/basic.go | target/debug/stdg[.exe]
+
+package main
+
+import "fmt"
+
+func main() {
+	for {
+		fmt.Println("color 255 255 255")
+		fmt.Println("clear")
+		fmt.Println("color 255 0 0")
+		fmt.Println("rect 50 50 100 100")
+		fmt.Println("present")
+		fmt.Println("handle")
+	}
+}
