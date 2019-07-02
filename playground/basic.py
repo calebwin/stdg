bfa9b2d2219b4cf4540ff62bfe9785a3e7a27a5b
calebwin <calebhwin@gmail.com>
2019-07-02 00:28:47 -0700

    added support for outlined shapes and circles

--- playground/basic.py
+++ playground/basic.py
@@ -4,9 +4,19 @@
 # 	python playground/basic.py | target/debug/stdg[.exe]
 
 while True:
+	# print("color 0 0 0 255")
+	# print("clear")
+	# print("color 255 0 0 255")
+	# print("rect 50 50 50 50")
 	print("color 255 255 255")
 	print("clear")
 	print("color 255 0 0")
-	print("rect 50 50 100 100")
+	print("fill circle 150 150 20")
+	print("point 0 300")
+	print("point 1 300")
+	print("point 2 300")
+	print("point 3 300")
+	print("point 4 300")
+	# print("circle 150 250 20")
 	print("present")
 	print("handle")
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-extern crate sdl2;
+// extern crate sdl2;
 
 use std::str::FromStr;
 
@@ -8,10 +8,28 @@ use std::io::BufRead;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
+use sdl2::rect::Point;
 use std::process;
 
 use sdl2::render::Renderer;
 use sdl2::Sdl;
+
+// extern crate cairo;
+
+// extern crate piston_window;
+
+// use piston_window::*;
+// use piston_window::rectangle::*;
+
+// use std::sync::mpsc::{Sender, Receiver};
+// use std::sync::mpsc;
+// use std::thread;
+
+// extern crate cairo;
+
+// use std::env;
+// use std::fs::File;
+// use cairo::{Context, Format, ImageSurface};
 
 /// Executes instructions to draw
 trait Executor {
@@ -54,7 +72,41 @@ impl<'a> Executor for Engine<'a> {
 					let _ = self.ren.set_draw_color(color);
 				}
 			}
-			InstructionType::Rect => {
+            InstructionType::FillSquare => {
+                if instruction.params.len() == 4 {
+                    // get parameters for rendering square
+                    let x = instruction.params[0].parse::<i32>().unwrap();
+                    let y = instruction.params[1].parse::<i32>().unwrap();
+                    let size = instruction.params[2].parse::<u32>().unwrap();
+
+                    // create square
+                    let mut rect = Rect::new(x, y, size, size);
+
+                    // render square
+                    let _ = self.ren.fill_rect(rect);
+                }
+            }
+            InstructionType::OutlineSquare => {
+                if instruction.params.len() == 4 {
+                    // get parameters for rendering square
+                    let x = instruction.params[0].parse::<i32>().unwrap();
+                    let y = instruction.params[1].parse::<i32>().unwrap();
+                    let size = instruction.params[2].parse::<i32>().unwrap();
+
+                    // create points
+                    let mut top_left = Point::new(x, y);
+                    let mut top_right = Point::new(x + size, y);
+                    let mut bottom_left = Point::new(x, y + size);
+                    let mut bottom_right = Point::new(x + size, y + size);
+
+                    // render rectangle
+                    let _ = self.ren.draw_line(top_left, top_right);
+                    let _ = self.ren.draw_line(top_right, bottom_right);
+                    let _ = self.ren.draw_line(bottom_right, bottom_left);
+                    let _ = self.ren.draw_line(bottom_left, top_left);
+                }
+            }
+			InstructionType::FillRect => {
 				if instruction.params.len() == 4 {
 					// get parameters for rendering square
 					let x = instruction.params[0].parse::<i32>().unwrap();
@@ -69,6 +121,153 @@ impl<'a> Executor for Engine<'a> {
 					let _ = self.ren.fill_rect(rect);
 				}
 			}
+            InstructionType::OutlineRect => {
+                if instruction.params.len() == 4 {
+                    // get parameters for rendering square
+                    let x = instruction.params[0].parse::<i32>().unwrap();
+                    let y = instruction.params[1].parse::<i32>().unwrap();
+                    let w = instruction.params[2].parse::<i32>().unwrap();
+                    let h = instruction.params[3].parse::<i32>().unwrap();
+
+                    // create points
+                    let mut top_left = Point::new(x, y);
+                    let mut top_right = Point::new(x + w, y);
+                    let mut bottom_left = Point::new(x, y + h);
+                    let mut bottom_right = Point::new(x + w, y + h);
+
+                    // render rectangle
+                    let _ = self.ren.draw_line(top_left, top_right);
+                    let _ = self.ren.draw_line(top_right, bottom_right);
+                    let _ = self.ren.draw_line(bottom_right, bottom_left);
+                    let _ = self.ren.draw_line(bottom_left, top_left);
+                }
+            }
+            InstructionType::Line => {
+                if instruction.params.len() == 4 {
+                    // get parameters for rendering square
+                    let x_1 = instruction.params[0].parse::<i32>().unwrap();
+                    let y_1 = instruction.params[1].parse::<i32>().unwrap();
+                    let x_2 = instruction.params[2].parse::<i32>().unwrap();
+                    let y_2 = instruction.params[3].parse::<i32>().unwrap();
+
+                    // create points
+                    let mut start = Point::new(x_1, y_1);
+                    let mut end = Point::new(x_2, y_2);
+
+                    // render line
+                    let _ = self.ren.draw_line(start, end);
+                }
+            }
+            InstructionType::FillCircle => {
+                if instruction.params.len() == 3 {
+                    // get parameters for rendering square
+                    let center_x = instruction.params[0].parse::<i32>().unwrap();
+                    let center_y = instruction.params[1].parse::<i32>().unwrap();
+                    let r = instruction.params[2].parse::<i32>().unwrap();
+
+                    for radius in 0..r {
+                        let diameter = radius * 2;
+
+                        let mut x = radius - 1;
+                        let mut y = 0;
+                        let mut tx = 1;
+                        let mut ty = 1;
+                        let mut error = tx - diameter;
+
+                        while x >= y {
+                            // create points
+                            let mut points = vec![
+                                Point::new(center_x + x, center_y - y),
+                                Point::new(center_x + x, center_y + y),
+                                Point::new(center_x - x, center_y - y),
+                                Point::new(center_x - x, center_y + y),
+                                Point::new(center_x + y, center_y - x),
+                                Point::new(center_x + y, center_y + x),
+                                Point::new(center_x - y, center_y - x),
+                                Point::new(center_x - y, center_y + x),
+                            ];
+
+                            // render points
+                            for point in points {
+                                let _ = self.ren.draw_point(point);
+                            }
+
+                            if error <= 0 {
+                                y += 1;
+                                error += ty;
+                                ty += 2;
+                            }
+
+                            if error > 0 {
+                                x -= 1;
+                                tx += 2;
+                                error += tx - diameter;
+                            }
+                        }
+                    }
+                }
+            }
+            InstructionType::OutlineCircle => {
+                if instruction.params.len() == 3 {
+                    // get parameters for rendering square
+                    let center_x = instruction.params[0].parse::<i32>().unwrap();
+                    let center_y = instruction.params[1].parse::<i32>().unwrap();
+                    let radius = instruction.params[2].parse::<i32>().unwrap();
+
+                    let diameter = radius * 2;
+
+                    let mut x = radius - 1;
+                    let mut y = 0;
+                    let mut tx = 1;
+                    let mut ty = 1;
+                    let mut error = tx - diameter;
+
+                    while x >= y {
+                        // create points
+                        let mut points = vec![
+                            Point::new(center_x + x, center_y - y),
+                            Point::new(center_x + x, center_y + y),
+                            Point::new(center_x - x, center_y - y),
+                            Point::new(center_x - x, center_y + y),
+                            Point::new(center_x + y, center_y - x),
+                            Point::new(center_x + y, center_y + x),
+                            Point::new(center_x - y, center_y - x),
+                            Point::new(center_x - y, center_y + x),
+                        ];
+
+                        // render points
+                        for point in points {
+                            let _ = self.ren.draw_point(point);
+                        }
+
+                        if error <= 0 {
+                            y += 1;
+                            error += ty;
+                            ty += 2;
+                        }
+
+                        if error > 0 {
+                            x -= 1;
+                            tx += 2;
+                            error += tx - diameter;
+                        }
+                    }
+                }
+            }
+            InstructionType::Point => {
+                if instruction.params.len() == 2 {
+                    // get parameters for rendering square
+                    let x = instruction.params[0].parse::<i32>().unwrap();
+                    let y = instruction.params[1].parse::<i32>().unwrap();
+
+                    // create point
+                    let mut rect = Point::new(x, y);
+
+                    // render point
+                    let _ = self.ren.draw_point(rect);
+                }
+            }
+
 			InstructionType::Clear => {
 				// clear
 				let _ = self.ren.clear();
@@ -124,7 +323,14 @@ struct Instruction {
 enum InstructionType {
 	Nothing,
 	Color,
-	Rect,
+    FillSquare,
+	FillRect,
+    FillCircle,
+    OutlineSquare,
+    OutlineRect,
+    OutlineCircle,
+    Point,
+    Line,
 	Clear,
 	Present,
 	Handle,
@@ -144,7 +350,18 @@ impl Instruction {
 		if tokens.len() >= 1 {
 			ty = match tokens[0].as_ref() {
 				"color" => InstructionType::Color,
-				"rect" => InstructionType::Rect,
+                "fill" => match tokens[1].as_ref() {
+                    "rect" => InstructionType::FillRect,
+                    "circle" => InstructionType::FillCircle,
+                    _ => InstructionType::Nothing,
+                },
+                "outline" => match tokens[1].as_ref() {
+                    "rect" => InstructionType::OutlineRect,
+                    "circle" => InstructionType::OutlineCircle,
+                    _ => InstructionType::Nothing,
+                },
+                "point" => InstructionType::Point,
+                "line" => InstructionType::Line,
 				"clear" => InstructionType::Clear,
 				"present" => InstructionType::Present,
 				"handle" => InstructionType::Handle,
@@ -154,7 +371,10 @@ impl Instruction {
 
 		// get parameters
 		if tokens.len() >= 2 {
-			params = tokens.clone()[1..].to_vec();
+			params = match tokens[0].as_ref() {
+                "fill" | "outline" => tokens.clone()[2..].to_vec(),
+                _ => tokens.clone()[1..].to_vec()
+            };
 		}
 
 		// return the new instruction
@@ -195,3 +415,174 @@ fn main() {
 	// run engine
 	engine.run();
 }
+
+// TODO print errors
+
+
+// fn main() {
+// // // 	// // channel for sending instructions
+// // // 	// let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+
+// // // 	// // thread for reading instructions
+// // // 	// let reader = thread::spawn(move || {
+// // // 	// 	// loop through input from stdin
+// // // 	//     for raw_line in io::stdin().lock().lines() {
+// // // 	//     	// parse line for instructions
+// // // 	//     	let line = raw_line.unwrap();
+// // // 	//     	let mut tokens = line.split_whitespace();
+
+// // // 	//     	// send instructions to printer
+// // // 	//     	tx.clone().send(tokens.next().unwrap().to_string()).unwrap();
+// // // 	//     }
+// // // 	// });
+
+// // // 	// // thread for printing graphics
+// // // 	// let printer = thread::spawn(move || {
+// // // 	// 	// create window with blank title and default dimensions
+// // // 	// 	let mut window: PistonWindow = WindowSettings::new("", [400, 400]).build().unwrap();
+
+// // // 	// 	while let Some(event) = window.next() {
+// // // 	//     	window.draw_2d(&event, |context, graphics, _device| {
+// // //  //    		    if let Ok(instruction) = rx.recv() {
+// // // 	// 				print!("{}", instruction);
+// // // 	// 		    }
+
+// // // 	//     		clear([1.0; 4], graphics);
+// // // 	//             rectangle([1.0, 0.0, 0.0, 1.0], // red
+// // // 	//                       [0.0, 0.0, 100.0, 100.0],
+// // // 	//                       context.transform,
+// // // 	//                       graphics);
+// // // 	//     	});
+// // // 	//     }
+// // // 	// });
+
+// // // 	// reader.join();
+// // // 	// printer.join();
+
+// // 	// create window
+// // 	let mut window: PistonWindow = WindowSettings::new("", [400, 400]).build().unwrap();
+
+// // 	// loop through input from stdin
+// //     for raw_line in io::stdin().lock().lines() {
+// //     	// parse line
+// //     	let line = raw_line.unwrap();
+// //     	let mut tokens = line.split_whitespace();
+
+// //     	// keep track of state
+// //     	let mut color: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+
+// //     	// handle instruction
+// //         match tokens.next().unwrap() {
+// //         	"color" => {
+// //         		let r: f32 = tokens.next().expect("expected amount of red in color").parse().expect("expected a floating point number");
+// //         		let g: f32 = tokens.next().expect("expected amount of green in color").parse().expect("expected a floating point number");
+// //         		let b: f32 = tokens.next().expect("expected amount of blue in color").parse().expect("expected a floating point number");
+// //         		let t: f32 = tokens.next().expect("expected amount of transparency").parse().expect("expected a floating point number");
+
+// //         		color = [r / 255f32, g / 255f32, b / 255f32, t / 255f32];
+// //         	}
+// //         	"clear" => {
+// // 				if let Some(event) = window.next() {
+// // 			        window.draw_2d(&event, |context, graphics, _device| {
+// // 			        	clear([0.0, 0.0, 0.0, 1.0], graphics);
+// // 			        });
+// // 			    }
+// //         	}
+// //         	"rect" => {
+// //         		let x: f64 = tokens.next().expect("expected x position of rectangle").parse().expect("expected a floating point number");
+// //         		let y: f64 = tokens.next().expect("expected y position of rectangle").parse().expect("expected a floating point number");
+// //         		let w: f64 = tokens.next().expect("expected width of rectangle").parse().expect("expected a floating point number");
+// //         		let h: f64 = tokens.next().expect("expected height of rectangle").parse().expect("expected a floating point number");
+
+// //        //  		println!("5");
+
+// //        //  		while let fn(piston_window::Loop) -> piston_window::Event {piston_window::Event::Loop} = Event::Loop {
+// // 			    //     window.draw_2d(&event, |context, graphics, _device| {
+// // 			    //         rectangle(color,
+// // 			    //                   [x, y, w, h],
+// // 			    //                   context.transform,
+// // 			    //                   graphics);
+// // 			    //     });
+// // 			    // }
+
+// // 			    let rectangle = Rectangle::new(color::BLACK);
+// // 				let dims = square(0.0, 0.0, 10.0);
+// // 				rectangle.draw(dims, &draw_state::DrawState::new_alpha(), transform, gfx_graphics::GfxGraphics::);
+// //         	}
+// //         	_ => {
+// //         		println!("bye!");
+// //         	}
+// //         }
+// //     }
+
+// 	// create window
+// 	let mut window: PistonWindow = WindowSettings::new("", [400, 400]).build().unwrap();
+
+
+// 	// keep track of some stuff
+// 	let mut color: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+
+// 	// loop
+// 	while let Some(e) = window.next() {
+
+// 		// parse a line of input
+//     	let mut line = String::new();
+//     	std::io::stdin().read_line(&mut line).unwrap();
+//     	let mut tokens = line.split_whitespace();
+
+//     	match tokens.next().unwrap().as_ref() {
+//     		"color" => {
+//         		let r: f32 = tokens.next().expect("expected amount of red in color").parse().expect("expected a floating point number");
+//         		let g: f32 = tokens.next().expect("expected amount of green in color").parse().expect("expected a floating point number");
+//         		let b: f32 = tokens.next().expect("expected amount of blue in color").parse().expect("expected a floating point number");
+//         		let t: f32 = tokens.next().expect("expected amount of transparency").parse().expect("expected a floating point number");
+
+//         		color = [r / 255f32, g / 255f32, b / 255f32, t / 255f32];
+//         	}
+//         	"clear" => {
+//         		window.draw_2d(&e, |c, g, _| {
+//         			clear(color, g);
+//         		});
+//         	}
+//     		"rect" => {
+// 				let x: f64 = tokens.next().expect("expected x position of rectangle").parse().expect("expected a floating point number");
+//     			let y: f64 = tokens.next().expect("expected y position of rectangle").parse().expect("expected a floating point number");
+//     			let w: f64 = tokens.next().expect("expected width of rectangle").parse().expect("expected a floating point number");
+//     			let h: f64 = tokens.next().expect("expected height of rectangle").parse().expect("expected a floating point number");
+
+// 				window.draw_2d(&e, |c, g, _| {
+//         			rectangle([1.0, 0.0, 0.0, 1.0], [x, y, w, h], c.transform, g);
+// 				});
+//     		}
+//     		"present" => {
+//     			// TODO present
+//     		}
+//     		_ => {}
+//     	}
+//     }
+// }
+
+
+// fn main() {
+// 	let mut app = simple::Window::new("hello world", 1920, 1080);
+
+// 	app.set_color(255, 0, 255, 255);
+// 	app.draw_rect(simple::Rect::new(
+// 	    100,
+// 	    110,
+// 	    120,
+// 	    130,
+// 	));
+
+// 	while app.next_frame() {}
+// }
+
+// pm/executive
+// - change supported platforms without changing development environment
+// - change development environment without changing supported platforms
+// - less coding
+// - only 1 tool for employees to learn for all projects
+
+// programmer
+// - print text
+// - print graphics
