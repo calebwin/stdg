753b3fd30c97e3cb263a53b586b8ac15153ca0c3
calebwin <calebhwin@gmail.com>
2019-07-02 13:21:33 -0700

    added GIF of moving car example

--- playground/basic.py
+++ playground/basic.py
@@ -3,20 +3,37 @@
 # 	cargo build
 # 	python playground/basic.py | target/debug/stdg[.exe]
 
+# x = 0
+
+# while True:
+# 	x += 10;
+
+# 	print("color 255 255 255")
+# 	print("clear")
+# 	print("color 255 20 20")
+# 	print("fill circle " + str(x) + " 150 80")
+# 	print("present")
+# 	print("handle")
+
+
+# position of the car
+x = 10
+
 while True:
-	# print("color 0 0 0 255")
-	# print("clear")
-	# print("color 255 0 0 255")
-	# print("rect 50 50 50 50")
-	print("color 255 255 255")
+	print("color 151 244 247")
 	print("clear")
-	print("color 255 0 0")
-	print("fill circle 150 150 80")
-	print("point 0 300")
-	print("point 1 300")
-	print("point 2 300")
-	print("point 3 300")
-	print("point 4 300")
-	# print("circle 150 250 20")
+
+	# draw the car body
+	print("color 255 0 115")
+	print("fill rect " + str(x) + " 200 100 20")
+	print("fill rect " + str(x + 15) + " 178 70 40")
+
+	# draw the wheels
+	print("color 77 66 66")
+	print("fill circle " + str(x + 25) + " 221 12");
+	print("fill circle " + str(x + 75) + " 221 12");
+
 	print("present")
 	print("handle")
+
+	x = x + 1
